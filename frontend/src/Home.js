37fc05611d3c2eb7901d66925088c9a87b2eca73
Michelle Shirk <michelle.l.shirk@gmail.com>
2023-02-15 16:33:11 -0500

    edit homepage styles

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import UserContext from "./UserContext";
-import './Home.css'
 
 function Home() {
     const { currentUser } = useContext(UserContext);
